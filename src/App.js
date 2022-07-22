cbc1a3f8765087e50eb9c91be6f3c625e6185c8a
Joy Paul <joypaul3592@gmail.com>
2022-07-22 17:10:04 +0600

    complete hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Component/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h1>Hello from development branch</h1>
+      <Homepage />
     </div>
   );
 }
--- src/Component/Homepage/Homepage.js
+++ src/Component/Homepage/Homepage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Guideline from './Guideline/Guideline';
+import Hero from './Hero/Hero.js'
 const Homepage = () => {
     return (
         <div>
             <h2>This is Guideline Component</h2>
+            <Hero />
         </div>
     );
 };
