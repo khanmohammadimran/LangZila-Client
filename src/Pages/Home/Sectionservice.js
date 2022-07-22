6110a9ea6cad44f104d269dd6aa3bbd453e0dba2
rajia124 <rajia5203@gmail.com>
2022-07-22 16:17:52 +0600

    add background image

--- src/Pages/Home/Sectionservice.js
+++ src/Pages/Home/Sectionservice.js
@@ -7,17 +7,18 @@ const Sectionservice = () => {
       <div className="full-app">
 
         <div className="container app-service">
-          <section>
+          <section className='text-section'>
             <h4 className='sm-tittle'>Our Services</h4>
             <h1 className='tittle'>Best Services for Kids</h1>
             <p>
 
               Explore all of our courses and pick your suitable ones to enroll and start learning with us! We ensure that you will never regret it!</p>
           </section>
-          <div>
+          <div className='slide-section'>
             <Service></Service>
           </div>
         </div>
+        <img className='back-img' src="kidsbg.png" alt="" />
       </div>
     </div>
   );
